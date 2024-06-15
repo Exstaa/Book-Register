67300cb287a0e7b742a0eb714701f0fee28d650e
Exstaa <exstagithub@gmail.com>
2024-06-15 19:12:06 +0400

    database connection created.

---
+++ az/developia/bookregister/database/Database.java
@@ -0,0 +1,38 @@
+package az.developia.bookregister.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Database {
+	  private final static String URL = "jdbc:mysql://localhost/sent6group";
+	    private final static String USERNAME = "root";
+	    private final static String PASSWORD = "root";
+	    private static Connection conn;
+
+	    public static Connection connect() {
+	        try {
+	            Class.forName("com.mysql.cj.jdbc.Driver");
+	        } catch (ClassNotFoundException ex) {
+	            ex.printStackTrace();
+	        }
+	        try {
+	            conn = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+	        } catch (SQLException ex) {
+	            ex.printStackTrace();
+	        }
+	        return conn;
+	    }
+
+	    public static void dissconnect() {
+	        try {
+	            conn.close();
+	        } catch (SQLException ex) {
+	            ex.printStackTrace();
+	        }
+	    }
+
+	    public static Connection getConn() {
+	        return conn;
+	    }
+}
